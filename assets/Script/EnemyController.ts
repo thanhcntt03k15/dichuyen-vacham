01491cfc06d1f15cfdda19cce1f2cb2d29b4e8cf
Thanh <thanhit03haui@gmail.com>
2022-11-02 20:07:39 +0700

    8:07PM 02/11/2002

---
+++ assets/Script/EnemyController.ts
@@ -0,0 +1,24 @@
+import {_decorator, Collider2D, Component, Contact2DType, IPhysics2DContact, Node, RigidBody2D, Vec2} from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('EnemyController')
+export class EnemyController extends Component {
+    protected start() {
+        //this.getComponent(RigidBody2D).linearVelocity = new Vec2(this.node.up.x * 500,this.node.up.y * 500);
+        let collider = this.getComponent(Collider2D);
+        if (collider)
+        {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.OnTriggerEnter, this);
+        }
+    }
+    OnTriggerEnter(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null){
+        console.log("OnTrigger");
+        otherCollider.node.active = false;
+        console.log("Log");
+    }
+    OffDesTroy()
+    {
+        this.node.destroy();
+    }
+}
+
