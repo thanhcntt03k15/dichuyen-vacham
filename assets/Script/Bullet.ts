dccbfeaa97e9c98933741baf5693f0dbc37e65b7
Thanh <thanhit03haui@gmail.com>
2022-11-02 20:07:39 +0700

    8:07PM 02/11/2002

--- assets/Script/Bullet.ts
+++ assets/Script/Bullet.ts
@@ -1,4 +1,14 @@
-import {_decorator, Component, Node, RigidBody2D, Vec2} from 'cc';
+import {
+    _decorator,
+    Collider2D,
+    Component,
+    Contact2DType, IPhysics2DContact,
+    Node,
+    Physics2DManifoldType,
+    PhysicsSystem2D,
+    RigidBody2D,
+    Vec2
+} from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Bullet')
@@ -9,6 +19,21 @@ export class Bullet extends Component {
 
     protected start() {
         this.getComponent(RigidBody2D).linearVelocity = new Vec2(this.node.up.x * 500,this.node.up.y * 500);
+        let collider = this.getComponent(Collider2D);
+        if (collider)
+        {
+            collider.on(Contact2DType.BEGIN_CONTACT, this.OnTriggerEnter, this);   
+        }
     }
+    OnTriggerEnter(selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact | null){
+        console.log("OnTrigger");
+        this.OffDesTroy();
+        console.log("Log");
+    }
+    OffDesTroy()
+    {
+        this.node.destroy();
+    }
+    
 }
 
